36f5fd96455be86a55aced517a1e180ac78019d1
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2022-04-03 09:57:29 +0200

    (bf) Bug RESOLVED / ##VERSION OK

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let matchNumber = 0;
 
 function flipCard() {
   if (lockBoard) return;
@@ -31,8 +32,12 @@ function checkForMatch() {
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
-  console.log('MATCH !!')
-
+  matchNumber++;
+  if(matchNumber === 8) {
+    setTimeout(() => {
+      winGame();
+    }, 500);
+  }
   resetBoard();
 }
 
@@ -51,16 +56,25 @@ function unflipCards() {
 // Reset Board
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null]
+  [firstCard, secondCard] = [null, null];
 }
 
 // Shuffle
 (function shuffle() {
   cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
+    let randomPos = Math.floor(Math.random() * 16);
     card.style.order = randomPos;
   })
 })();
+
+// Wingame
+function winGame() {
+  cards.forEach(card => {
+    setTimeout(() => {
+      card.classList.add('fade-out');
+    }, 1000);
+  })
+}
 
 // Loop on cards with a forEach (querySelectorAll only not enough)
 cards.forEach(card => card.addEventListener('click', flipCard))
